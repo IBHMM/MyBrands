41252982203fd9f8b5b7b1429ea986b9da2b6854
Ibrahim Muradov <136456526+IBHMM@users.noreply.github.com>
2024-05-27 22:03:01 +0400

    Cart

--- src/components/products/ProductOpenMain.jsx
+++ src/components/products/ProductOpenMain.jsx
@@ -4,8 +4,9 @@ import greentruck from '../../assets/products/greentruck.png'
 import right from '../../assets/home/rightT.png'
 import card from '../../assets/products/card.png'
 import like from '../../assets/products/like.png'
-import addProduct from '../../features/User/UserSlice'
+import { removeProduct, addProduct } from "../../features/User/UserSlice";
 import RatingSize from "./Layout/Rating";
+import liked from '../../assets/products/likedC.png';
 
 function ProductOpenMain() {
     const product = useSelector(state => state.user.ActiveProduct);
@@ -23,13 +24,26 @@ function ProductOrderPart({product}) {
     const [activesrc, setActiveSrc] = useState(product.images[0]);
     const [activecolor, setActiveColor] = useState(product.images[0]);
     const [activesize, setActiveSize] = useState({size: '', aviable: ''})
+    const wishlist = useSelector(state => state.user.wishlist);
+    const [isliket, setIsliked] = useState(wishlist.some(prd => prd.id == product.id))
     const dispatch = useDispatch();
 
-    return (
-        <section className="w-full flex items-center justify-start px-3 py-2 max-h-full">
+    const HandleLike = () => {
+        if (isliket) {
+            setIsliked(!isliket);
+            dispatch(removeProduct(product));
+        }else {
+            setIsliked(!isliket);
+            dispatch(addProduct(product));
+        }
+    }
 
-            <section className="flex items-center justify-between w-[609px]">
-                <section className="flex flex-col items-start justify-start gap-[10px] h-[611px]">
+
+    return (
+        <section className="w-full flex items-center justify-start px-3 py-2 max-h-full max-[1100px]:flex-col">
+
+            <section className="flex items-center justify-between min-w-[609px] max-[1100px]:w-full">
+                <section className="flex flex-col items-start justify-start gap-[10px] h-[611px] max-[1300px]:hidden">
                     {
                         product.images.map((src, index) => {
                             return (
@@ -38,18 +52,28 @@ function ProductOrderPart({product}) {
                         })
                     }
                 </section>
-                <section>
-                    <img src={activesrc} alt="" className="w-[506px] h-[611px]" />
+                <section className="flex w-full items-center justify-center overflow-x-auto">
+
+                    <img src={activesrc} alt="" className="min-[1100px]:w-[506px] h-[611px] max-[1100px]:max-w-full max-[1100px]:hidden" />
+                    <div className="flex w-full h-full items-center justify-start min-[1100px]:hidden">
+                        {
+                            product.images.map((picture, index) => {
+                                return (
+                                    <img src={picture} key={index}/>
+                                )
+                            }) 
+                        }
+                    </div>
                 </section>
             </section>
 
-            <section className="flex flex-col w-[612px] items-start justify-start h-[611px]">
+            <section className="w-full flex flex-col items-start justify-start h-[611px]">
                 <p className="text-[16px] text-[#9B96B7]">{product.brand}</p>
                 <p className="text-[20px] text-[#292D32]">{product.title}</p>
                 <RatingSize value={product.rating}/>
                 <p className="text-[24px] text-[#292D32]">{product.price} AZN</p>
                 
-                <div className="w-[400px] h-[77px] border border-[#14AE5C] bg-[#14AE5C] bg-opacity-5 flex flex-col items-start justify-center pl-5 mt-[30px]">
+                <div className="w-[340px] h-[77px] border border-[#14AE5C] bg-[#14AE5C] bg-opacity-5 flex flex-col items-start justify-center pl-5 mt-[30px]">
                     <div className="flex items-center gap-[10px]">
                         <p className="text-[#14AE5C] text-[18px]">Pulsuz çatdırılma!</p>
                         <img src={greentruck} alt="" />
@@ -73,7 +97,7 @@ function ProductOrderPart({product}) {
                     </div>
                 </div>
 
-                <div className="w-[328px] flex flex-col items-start justify-start gap-[15px] mt-[30px]">
+                <div className=" flex flex-col items-start justify-start gap-[15px] mt-[30px]">
                         <div className="w-full flex items-center justify-between ">
                             <p className="text-[#292D32] text-[16px] font-semibold">Bədən:</p>
                             <button className="flex items-center justify-between gap-[7px] text-[14px] text-[#484C52]">
@@ -103,12 +127,12 @@ function ProductOrderPart({product}) {
                 </div>
 
                 <div className="flex items-center justify-between w-full h-10 mt-[30px] gap-[10px]">
-                    <button className="w-[548px] h-[48px] bg-[#26264C] text-white flex items-center justify-center gap-[10px] text-[16px] text-semibold">
+                    <button className="w-[90%] h-[48px] bg-[#26264C] text-white flex items-center justify-center gap-[10px] text-[16px] text-semibold transition-all duration-300 hover:rounded-xl active:scale-90">
                         <img src={card} alt="" />
                         {'Səbətə əlavə et'.toUpperCase()}
                     </button>
-                    <button className="border border-[#ECECEE] flex items-center justify-center bg-[#FAFAFA] w-[48px] h-[48px]">
-                        <img src={like} alt="" />
+                    <button className={`border border-[#ECECEE] flex items-center justify-center bg-[#FAFAFA] w-[48px] h-[48px] transition-all duration-300 hover:scale-80 ${isliket ? 'border-[0px] bg-white' : 'flex'}`} onClick={HandleLike}>
+                        <img src={isliket ? liked : like} alt="" />
                     </button>
                 </div>
             </section>
@@ -127,7 +151,7 @@ function AboutProduct({product}) {
             </div>
             <div className="w-full flex flex-col items-start justify-start">
                 <p className="text-[18px] py-3">Məhsulun kodu: {product.sku}</p>
-                <ul className="text-[#484C52] text-[16px]">
+                <ul className="text-[#484C52] text-[16px]" >
                     <li>- {product.description}</li>
                     <li>- {product.title}</li>
                     <li>- {product.category}</li>
