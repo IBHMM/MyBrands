e13f20ad307bd03cd66febe006bf306b318ea887
Ibrahim Muradov <136456526+IBHMM@users.noreply.github.com>
2024-05-31 12:03:08 +0400

    bugfixxing2

--- src/pages/Profile/FinishOrder.jsx
+++ src/pages/Profile/FinishOrder.jsx
@@ -2,7 +2,7 @@ import Navbar from "../../components/home/Layout/Navbar"
 import Header from "../../components/home/Layout/Header"
 import Footer from "../../components/home/Layout/Footer"
 import WisitedPages from '../../components/home/Layout/WisitedPages'
-import OrderMain from '../../components/Profile/OrdersMain.jsx'
+import OrderFinishMain from '../../components/Profile/OrdersFinishMain.jsx'
 
 
 function FinishOrder () {
@@ -12,7 +12,7 @@ function FinishOrder () {
             <Navbar />
             <Header />
             <WisitedPages pages={["Home", "Profile"]}/>
-            <OrderMain />
+            <OrderFinishMain />
             <Footer />
         </section>
     )
